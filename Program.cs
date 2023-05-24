604120ed3d384c1d62373313f034eb564d438415
Györfi Marcell <gyorfimarcell@gmail.com>
2023-05-24 22:53:34 +0200

    4. feladat

--- Program.cs
+++ Program.cs
@@ -5,5 +5,16 @@
         List<Kifejezes> kifejezesek = File.ReadAllLines("kifejezesek.txt").Select(x => new Kifejezes(x)).ToList();
 
         Console.WriteLine($"2. feladat: Kifejezések száma: {kifejezesek.Count}");
+
+        Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {kifejezesek.Count(x => x.Muvelet == "mod")}");
+
+        if (kifejezesek.Any(x => x.ElsoOperandus % 10 == 0 && x.MasodikOperandus % 10 == 0))
+        {
+            Console.WriteLine("4. feladat: Van ilyen kifejezés");
+        }
+        else
+        {
+            Console.WriteLine("4. feladat: Van ilyen kifejezés");
+        }
     }
 }
